b850a653963edc68594459b92e8c8e2f72eae45c
Nurattin <hhhhghhghu@gmail.com>
2024-02-10 12:32:27 +0300

    Добавил необходимые зависимотси

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,23 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath(libs.kotlin.serialization)
+        classpath(libs.kotlin.gradle)
+        classpath(libs.google.dagger)
+    }
+}
+
 plugins {
-    id("com.android.application") version "8.2.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    alias(libs.plugins.jetbrains.android) apply false
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.android.library) apply false
+}
+tasks {
+    register("clean", Delete::class) {
+        delete(rootProject.buildDir)
+    }
 }
